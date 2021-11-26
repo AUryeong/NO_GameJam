4501a50468254ffee4cd927b0bc7b04023fcf35b
Black_Penguin <dkstmdwnswns@gmail.com>
2021-11-26 23:23:58 +0900

    SoundManager + Singleton

---
+++ Assets/SingletonObject.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonObject : Singleton<SingletonObject>
+{
+    protected SingletonObject() { }
+}
---
+++ Assets/SoundManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Clip
+{
+    public string Name;
+    public AudioClip clip;
+}
+public class SoundManager: Singleton<SoundManager>
+{
+    public AudioSource audioSource;
+    public AudioSource audioSE;
+    public List<Clip> clips = new List<Clip>();
+    protected SoundManager() { }
+
+    public void ChangeClip(string name, bool loop)
+    //사용법 Sound.Instance.ChangeClip("이름",루프 할껀지안할껀지(bool))
+    {
+        Clip find = clips.Find((o) => { return o.Name == name; });
+        if (find != null)
+        {
+            if (loop == true)
+            {
+                audioSource.Stop();
+                audioSource.clip = find.clip;
+                audioSource.loop = true;
+                audioSource.Play();
+            }
+            else if (loop == false)
+            {
+                audioSE.clip = find.clip;
+                audioSE.loop = false;
+                audioSE.Play();
+
+            }
+        }
+    }
+    public void SetMusicVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+    public void SetSEVolume(float volume)
+    {
+        audioSE.volume = volume;
+    }
+}
