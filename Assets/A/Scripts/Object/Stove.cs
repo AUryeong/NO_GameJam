d7fc2e765267d121187e251a787d2562b4ed24c3
AUryeong <jykee7030@gmail.coma>
2021-11-27 11:18:04 +0900

    BaseObject 생성 및 여러개들 기반 제작

---
+++ Assets/A/Scripts/Object/Stove.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stove : BaseObject
+{
+    public override void OnHammer()
+    {
+        base.OnHammer();
+    }
+    public override void OnMetch()
+    {
+        base.OnMetch();
+    }
+}
