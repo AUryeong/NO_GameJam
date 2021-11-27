c10056803d3ee3895404eba9d0ef429a6ac6d703
AUryeong <jykee7030@gmail.coma>
2021-11-27 11:41:01 +0900

    Stove 변경

--- Assets/A/Scripts/Object/Stove.cs
+++ Assets/A/Scripts/Object/Stove.cs
@@ -4,12 +4,49 @@ using UnityEngine;
 
 public class Stove : BaseObject
 {
+    public static int MaxGage = 20;
+    public int Gage = 0;
+    public bool active = true;
+    private float activeduration;
+    private float duration;
+
+    void Update()
+    {
+        if (active)
+        {
+            if (Gage <= 0)
+            {
+                active = false;
+            }
+            else
+            {
+                duration += Time.deltaTime;
+                if (duration >= 1)
+                {
+                    Gage += Random.Range(2, 5);
+                }
+                if(Gage >= 20)
+                {
+                    //여기다가 게임오버 넣어주세요
+                }
+            }
+        }
+        else
+        {
+            activeduration += Time.deltaTime;
+            if (activeduration >= 5)
+            {
+                active = true;
+                duration = 0;
+            }
+        }
+    }
     public override void OnHammer()
     {
-        base.OnHammer();
+        Gage = Mathf.Max(0, Gage-5);
     }
     public override void OnMetch()
     {
-        base.OnMetch();
+        Gage = Mathf.Min(MaxGage, Gage + 5);
     }
 }
