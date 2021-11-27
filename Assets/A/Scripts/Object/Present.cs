6154773e236399289df09054a4acbfdec65d39bd
AUryeong <jykee7030@gmail.coma>
2021-11-27 15:23:18 +0900

    수정?

--- Assets/A/Scripts/Object/Present.cs
+++ Assets/A/Scripts/Object/Present.cs
@@ -4,21 +4,20 @@ using UnityEngine;
 
 public class Present : BaseObject
 {
+    public Sprite defaultsprite;
+    public Sprite opensprite;
     void Start()
     {
-        OnMetch();
+        gameObject.GetComponent<SpriteRenderer>().sprite = defaultsprite;
     }
     public override void OnHammer()
     {
         base.OnHammer();
+        gameObject.GetComponent<SpriteRenderer>().sprite = opensprite;
     }
     public override void OnMetch()
     {
         base.OnMetch();
-        GameObject obj = GameObject.Instantiate<GameObject>(Resources.Load<GameObject>("Fire"), gameObject.transform.parent);
-        obj.transform.localPosition = gameObject.transform.localPosition;
-        obj.transform.localScale = gameObject.transform.localScale / 14;
-        obj.transform.localRotation = gameObject.transform.localRotation;
         GameObject.Destroy(gameObject);
     }
 }
