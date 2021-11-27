aee9d38ad6a772560fbfaa112fd8819ec6b02d37
Black_Penguin <dkstmdwnswns@gmail.com>
2021-11-28 01:36:13 +0900

    data

--- Assets/Black_Penguin/Scripts/Data.cs
+++ Assets/Black_Penguin/Scripts/Data.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class Data : Singleton<Data>
 {
+    public int score;
+    public int stage;
 
+    void Highscoreset(int value)
+    {
+        PlayerPrefs.SetInt("Highscore",value);
+    }
+    void Stageset(int value)
+    {
+        PlayerPrefs.SetInt("Stage", value);
+    } 
 }
